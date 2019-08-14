725d95b65a6687b5b8edc94f2e78c6dbcf62e401
Zhangxu <738285867@qq.com>
2019-08-14 17:45:55 +0800

    add a template and d3.js

--- index.py
+++ index.py
@@ -1,17 +1,18 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for
 from model.segment import segment_3_parts
 import json
-app = Flask(__name__)
-
+app = Flask(__name__, static_url_path="")
+# url_for('static', filename='static')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
-        return render_template('index.html')
+        return render_template('index_naive.html')
     else:
         speech = request.form['speech']
         extration_result = segment_3_parts(speech)
-        return render_template('index.html', **extration_result)
+        return render_template('index_naive.html', **extration_result)
+
 
 @app.route('/extration/<speech>/', methods=['GET'])
 def extration(speech):
